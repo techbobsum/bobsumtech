4e3b3018b9aaf2f09f9513244dd4e7879b50e0c1
Gracious Sithole <85287261+techbobsum@users.noreply.github.com>
2021-06-05 14:19:46 +0200

    Delete equitex directory

--- equitex/src/main/java/com/bobsumsol/equitex/repository/SubscriberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bobsumsol.equitex.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bobsumsol.equitex.models.Subscriber;
-
-public interface SubscriberRepository extends JpaRepository<Subscriber, Long> {
-	public Subscriber findByEmail(String email);
-}
